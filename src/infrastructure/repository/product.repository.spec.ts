c36b5cc346baf6485e889d6bb4b1c15f3bfc300a
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2023-11-19 16:05:36 -0300

    init repository tests config

---
+++ src/infrastructure/repository/product.repository.spec.ts
@@ -0,0 +1,23 @@
+import { Sequelize } from "sequelize-typescript"
+import ProductModel from "../db/sequelize/model/product.model";
+
+
+describe("Product repository tests", () => {
+  let sequelize: Sequelize;
+
+  beforeEach(async () => {
+    sequelize = new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory:",
+      logging: false,
+      sync: { force: true }
+    })
+
+    sequelize.addModels([ProductModel])
+    await sequelize.sync()
+  });
+
+  afterEach(async () => {
+    await sequelize.close();
+  })
+})
