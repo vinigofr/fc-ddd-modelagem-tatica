5b035b1950ff41dbd14e48b4fae74919cbabd9f9
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2023-12-17 17:23:52 -0300

    fix error in event.dispatcher.interface.ts

--- src/domain/event/@shared/event.dispatcher.interface.ts
+++ src/domain/event/@shared/event.dispatcher.interface.ts
@@ -4,6 +4,6 @@ import EventInterface from "./event.interface";
 export default interface EventDispatcherInterface {
   notify(event: EventInterface): void;
   register(eventName: string, eventHandler: EventHandlerInterface): void;
-  unregister(eventName: string, eventHandler: EventDispatcherInterface): void;
+  unregister(eventName: string, eventHandler: EventHandlerInterface): void;
   unregisterAll(): void
 }
