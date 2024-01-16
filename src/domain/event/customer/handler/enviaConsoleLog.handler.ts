d181e5725175264ae740b5ef8b1266ec7694563a
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2024-01-15 23:51:48 -0300

    fix: class implementation

--- src/domain/event/customer/handler/enviaConsoleLog.handler.ts
+++ src/domain/event/customer/handler/enviaConsoleLog.handler.ts
@@ -1,10 +1,11 @@
 import EventHandlerInterface from "../../@shared/event.handler.interface";
 import EventInterface from "../../@shared/event.interface";
-import CustomerCreatedEvent from "../customerCreated.event";
+import CustomerAddressChangedEvent from "../customerAddressChanged.event";
 
 export default
   class EnviaConsoleLogHandler
-  implements EventHandlerInterface<CustomerCreatedEvent> {
+  implements EventHandlerInterface<CustomerAddressChangedEvent> {
+  // Baaaita olhos de águia! Obrigado pelo apontamento
 
   handler(event: EventInterface): void {
     console.log("address changed to:", event.eventData.address);
